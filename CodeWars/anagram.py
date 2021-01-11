00d416d5216d8a9f17b5a63c57e1397d2936a386
Benny Ruiz Jimenez <A01328177@itesm.mx>
2021-01-10 18:01:06 -0600

    10/365

---
+++ CodeWars/anagram.py
@@ -0,0 +1,33 @@
+# An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+# Note: anagrams are case insensitive
+
+# Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+# 
+
+def is_anagram(test, original):
+    
+    ### Check length
+    
+    if len(test) != len(original):
+        return False
+    
+    ### Sort the word
+    
+    test = test.upper()
+    original = original.upper()
+    
+    
+    test = list(test)
+    original = list(original)
+    
+    test.sort()
+    original.sort()
+    
+    for i in range(0,len(original)):
+        if test[i] != original[i]:
+            return False
+    
+    
+    return True
