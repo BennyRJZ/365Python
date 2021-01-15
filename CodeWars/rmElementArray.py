4cfd4227eff6d132f112c156299b429c4cc2f914
Benny Ruiz Jimenez <A01328177@itesm.mx>
2021-01-14 22:08:47 -0600

    14/365 CORRECCION

--- CodeWars/rmElementArray.py
+++ CodeWars/rmElementArray.py
@@ -1,25 +1,17 @@
-# Your goal in this kata is to implement a difference function, which subtracts one list from 
-# another and returns the result.
-
-# It should remove all values from list a, which are present in list b.
-
-# array_diff([1,2],[1]) == [2]
-# If a value is present in b, all of its occurrences must be removed from the other:
-
-# array_diff([1,2,2,2,3],[2]) == [1,3]
-
 def array_diff(a, b):
     if len(a) == 0:
         return a
     elif len(b) == 0:
         return a
-    else:
-        value = b[0]
-    aux = a
-    a = []
-    for x in aux:
-        if x != value:
-             a.append(x)
+    for i in b:
+        value = i
+        aux = a
+        a = []
+        for x in aux:
+            if x != value:
+                a.append(x)
+        print(a)
+                
     
     return(a)
     #your code here
