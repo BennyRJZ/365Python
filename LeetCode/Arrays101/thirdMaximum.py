ae928a4e3fc46859890d9bfb376caba5d3dcdacb
Benny Ruiz Jimenez <A01328177@itesm.mx>
2021-02-02 23:09:53 -0600

    32/365

---
+++ LeetCode/Arrays101/thirdMaximum.py
@@ -0,0 +1,38 @@
+# Given integer array nums, return the third maximum number in this array. 
+# If the third maximum does not exist, return the maximum number.
+
+ 
+
+# Example 1:
+
+# Input: nums = [3,2,1]
+# Output: 1
+# Explanation: The third maximum is 1.
+# Example 2:
+
+# Input: nums = [1,2]
+# Output: 2
+# Explanation: The third maximum does not exist, so the maximum (2) is returned instead.
+# Example 3:
+
+# Input: nums = [2,2,3,1]
+# Output: 1
+# Explanation: Note that the third maximum here means the third maximum distinct number.
+# Both numbers with value 2 are both considered as second maximum.
+ 
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums = set(nums)
+        print(nums)
+        nums = list(nums)
+        nums.sort()
+        
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return nums[1]
+        if len(nums) >= 3:
+            return nums[-3]
+        
+        
+        
