ddd29d2051caedb1f99ef8c23aec3b92519b97e1
Benny Ruiz Jimenez <A01328177@itesm.mx>
2021-02-19 23:20:25 -0600

    47/365

---
+++ Pandas/transposeDF.py
@@ -0,0 +1,12 @@
+#this program transpose a dataframe (rows to columns, columns to rwos)
+
+import pandas as pd
+file = "/Users/benny/Documents/LabFutbol/Laterales.csv"
+df = pd.read_csv(file)
+# print(df.head())
+print(df.dtypes)
+df_T = df.T
+print(df.dtypes)
+
+print(df.head())
+df_T.to_csv("pivotTabla.csv")
