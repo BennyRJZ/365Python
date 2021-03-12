a6cb621516e199dfbcb9e563413a1339861abd7f
Benny Ruiz Jimenez <A01328177@itesm.mx>
2021-03-11 23:55:12 -0600

    59/365

---
+++ LeetCode/Arrays101/heightchecker.py
@@ -0,0 +1,17 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        pivot = 0
+        counter = 0
+        for i in range(0,len(heights)-1):
+            if heights[i]> heights[i+1]:
+                pivot = heights[i+1]
+                heights[i+1] = heights[i]
+                heights[i] = pivot
+                i = 0
+                counter = counter + 1
+                print(counter)
+        print()
+        
+        return counter
+                
+            
